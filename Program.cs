0dc9fcc78073b02daf6c160cdb6c0860ce9fd8c7
Dmirty <9048881951@mail.ru>
2022-08-21 12:07:51 +0500

    008

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//#1
+/*
+int[] array = new int[100];
+int arraySize = array.Length;
+int count=0;
+for (int i = 0; i < arraySize; i++)
+{
+	array[i] = new Random().Next(100, 1000);
+	if (array[i]%2==0) count++;
+}
+//Console.WriteLine($" {string.Join("; ", array)} ");
+Console.WriteLine($"Количество четных чисел массива = {count}");
+*/
+//№2
+/*
+int[] array = new int[10];
+int arraySize = array.Length;
+int count = 0;
+for (int i = 0; i < arraySize; i++)
+{
+	array[i] = new Random().Next(10);
+	if (i != 0 && i % 2 != 0) count += array[i];
+}
+Console.WriteLine($" {string.Join("; ", array)} ");
+Console.WriteLine($"Сумма чисел на нечетных поцизиях массива = {count}"); //Позиция = индекс числа в массиве!
+*/
+
+//#3
+
+double[] array = new double[10];
+double arraySize = array.Length;
+double maxNum = 0;
+double minNum = 100;
+for (int i = 0; i < arraySize; i++)
+{
+	array[i] = Math.Round(new Random().NextDouble()*100,2);
+	if (array[i]>maxNum) maxNum=array[i];
+	if (array[i]<minNum) minNum=array[i];
+}
+//Console.WriteLine($" {string.Join("; ", array)} максимум - {maxNum} минимум - {minNum}");
+System.Console.WriteLine($"Разница максимального {maxNum} и минимального {minNum} чисел массива = {maxNum-minNum}");
